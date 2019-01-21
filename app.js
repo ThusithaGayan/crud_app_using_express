c2c7ef6aa13ea5aa75536333589745898d7cb904
ThusithaGayan <thusitharmtgk@gmail.com>
2019-01-21 21:35:01 +0530

    CRUD is complete,css not complete

--- app.js
+++ app.js
@@ -33,6 +33,19 @@ app.get("/",function(req,res){
 app.get("/create",function(req,res){
     res.render("create");
 });
+
+app.post("/create",function(req,res){
+    var name=req.body.name;
+    var age=req.body.age;
+    var sql="Insert into student values('"+name+"','"+age+"');";
+    pool.query(sql,function(err,res2,cols){
+        if(err){
+            throw err;
+        }
+        res.redirect("/retrive");
+    });
+});
+
 app.get("/retrive",function(req,res){
     var sql="select*from student";
     pool.query(sql,function(err,res2,cols){
@@ -45,24 +58,61 @@ app.get("/retrive",function(req,res){
     });
     
 });
-app.get("/update",function(req,res){
-    res.render("update");
-});
-app.get("/delete",function(req,res){
-    res.render("update");
-});
 
-app.post("/create",function(req,res){
-    var name=req.body.name;
-    var age=req.body.age;
-    var sql="Insert into student values('"+name+"','"+age+"');";
+app.get("/update",function(req,res){
+    var sql="select*from student";
     pool.query(sql,function(err,res2,cols){
         if(err){
             throw err;
         }
-        res.render("create");
+        //console.log(res2[0].name);
+        res.render("update",{result:res2});
+
     });
+
+    //res.render("update");
 });
+
+app.post("/update",function(req,res){
+    var name=req.body.name;
+    var age=req.body.age;
+    var sql="update student set age='"+age+"' where name='"+name+"';";
+    pool.query(sql,function(err,res2,cols){
+        if(err){
+            throw err;
+        }
+        res.redirect("/retrive");
+    });
+    //console.log(req.body);
+
+});
+
+app.get("/delete",function(req,res){
+    var sql="select*from student";
+    pool.query(sql,function(err,res2,cols){
+        if(err){
+            throw err;
+        }
+        //console.log(res2[0].name);
+        res.render("delete",{result:res2});
+
+    });
+    
+});
+
+
+app.post("/delete",function(req,res){
+    var name=req.body.name;
+    var sql="delete from student where name='"+name+"';";
+    pool.query(sql,function(err,res2,cols){
+        if(err){
+            throw err;
+        }
+        res.redirect("/retrive");
+    });
+
+});
+
 
 app.listen(8080,function(){
     console.log("server has been started");
